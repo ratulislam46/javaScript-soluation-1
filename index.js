5b9fd52836846533e765790f4e875330279a1aef
ratulislam46 <arfanratul46@gmail.com>
2025-08-25 23:17:16 +0600

    soluation two js question

---
+++ index.js
@@ -0,0 +1,28 @@
+// function abc(fn) {
+//     callBack()
+// }
+
+// function callBack() {
+//     console.log('I am a call back function');
+// }
+
+// abc("fn")
+
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 5];
+// function duplicate(array) {
+//     let newArray = [];
+//     let n = array.length;
+//     for (let i = 0; i < n; i++) {
+//         for (let j = i + 1; j < n; j++) {
+//             if (array[i] == array[j]) {
+//                 newArray.push(array[i]);
+//             }
+//         }
+//     }
+//     return newArray;
+// }
+
+// console.log(duplicate(array));
+
+
